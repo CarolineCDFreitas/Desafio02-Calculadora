522e37e9582b62a90b87f44346a5d02cf0db2112
CarolineCDFreitas <117691574+CarolineCDFreitas@users.noreply.github.com>
2024-11-07 01:12:32 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import * as S from "./style";
+
+export default function App() {
+  const [primeiroValor, setPrimeiroValor] = useState("");
+  const [segundoValor, setSegundoValor] = useState("");
+  const [resultado, setResultado] = useState("");
+
+  //função genérica para conter a lógica junto com o filtro, a verificação desse filtro e a seubsequente conversão em número
+  const capturandoValores = (e, setValor) => {
+    const valor = e.target.value;
+
+    //método para filtrar os valores. necessário por conta do type="text", para evitar NaN;
+    const ehNumero = [...valor].filter(
+      (caracteres) => !isNaN(caracteres) && caracteres !== ""
+    );
+
+    //condicional para verificar se tem número, e se sim juntaá-los e convertê-los em números para as operações aritméticas.
+    const valorFiltrado = ehNumero.length > 0 ? Number(ehNumero.join("")) : "";
+
+    setValor(valorFiltrado);
+  };
+
+  const capturandoPrimeiroValor = (e) => {
+    capturandoValores(e, setPrimeiroValor);
+  };
+
+  const capturandoSegundoValor = (e) => {
+    capturandoValores(e, setSegundoValor);
+  };
+
+  //funções que fazem as operações e depois convertem os resultados para strings
+  const soma = () => {
+    const resultadoSoma = primeiroValor + segundoValor;
+    setResultado(String(resultadoSoma));
+  };
+
+  const subtracao = () => {
+    const resultadoSubtracao = primeiroValor - segundoValor;
+    setResultado(String(resultadoSubtracao));
+  };
+
+  const multiplicacao = () => {
+    const resultadoMultiplicacao = primeiroValor * segundoValor;
+    setResultado(String(resultadoMultiplicacao));
+  };
+
+  //verficação do segundo valor, caso contrário qualquer número dividido por 0 dá como resultado Infinity
+  const divisao = () => {
+    const resultadoDivisao = primeiroValor / segundoValor;
+    segundoValor === 0
+      ? (alert("Não é possível dividir por 0."), handleClear())
+      : setResultado(String(resultadoDivisao));
+  };
+
+  //função adicional para realizar a operação de porcentagem
+  const porcentagem = () => {
+    const resultadoPorcentagem = (primeiroValor * segundoValor) / 100;
+    setResultado(String(resultadoPorcentagem));
+  };
+
+  //função adicional que reseta os campos indicados
+  const handleClear = () => {
+    setPrimeiroValor("");
+    setSegundoValor("");
+    setResultado("");
+  };
+
+  return (
+    <S.Main>
+      <S.GlobalStyle />
+      <S.Titulo>Calculadora</S.Titulo>
+      <S.ConteinerInput>
+        <S.Input
+          type="text"
+          inputMode="numeric"
+          value={primeiroValor}
+          placeholder="Digite o primeiro número"
+          onChange={capturandoPrimeiroValor}
+        />
+        <S.Input
+          type="text"
+          inputMode="numeric"
+          value={segundoValor}
+          placeholder="Digite o segundo número"
+          onChange={capturandoSegundoValor}
+        />
+      </S.ConteinerInput>
+      <S.ConteinerButton>
+        <S.Button onClick={handleClear}>C</S.Button>
+        <S.Button onClick={soma}>+</S.Button>
+        <S.Button onClick={subtracao}>-</S.Button>
+        <S.Button onClick={multiplicacao}>x</S.Button>
+        <S.Button onClick={divisao}>/</S.Button>
+        <S.Button onClick={porcentagem}>%</S.Button>
+      </S.ConteinerButton>
+      <S.Resultado>{resultado}</S.Resultado>
+    </S.Main>
+  );
+}
---
+++ src/style.js
@@ -0,0 +1,109 @@
+import { styled, createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+#root, html {
+  height: 100%;
+}
+
+body {
+    background-color: #b3e800;
+    border: 3px solid #fff;
+    height: 100%;
+}
+`;
+
+const FlexboxCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Main = styled.main`
+  ${FlexboxCenter};
+  flex-direction: column;
+  height: 100%;
+  margin: 0 0.2em;
+`;
+
+export const ConteinerInput = styled.section`
+  ${FlexboxCenter};
+  margin: 1.5em;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
+export const ConteinerButton = styled.section`
+  ${FlexboxCenter};
+  flex-wrap: wrap;
+  gap: 0.5rem 1.25rem;
+  max-width: 950px;
+  width: 100%;
+  padding: 0.5em;
+
+  @media (max-width: 600px) {
+    max-width: 300px;
+  }
+`;
+
+export const Titulo = styled.h1`
+  font-size: 3rem;
+  color: #48346e;
+  text-align: center;
+  text-shadow: 2px 2px 5px #f5fbe0;
+`;
+
+export const Input = styled.input`
+  font-size: 1.4rem;
+  text-align: center;
+  margin: 0.5rem;
+  padding: 0.5em;
+  border: none;
+  border-radius: 3rem;
+
+  &:focus {
+    border: 2px solid #8e00e8;
+    outline: none;
+    box-shadow: 0 0 5px rgba(142, 0, 232, 0.8);
+  }
+`;
+
+export const Resultado = styled.p`
+  font-size: 2rem;
+  text-align: center;
+  margin: 0.5em;
+  min-height: 3rem;
+`;
+
+export const Button = styled.button`
+  font-size: 1.4rem;
+  text-align: center;
+  color: #2f0b0b;
+  min-width: 5rem;
+  padding: 0.2em;
+  background-color: #c5f1f1;
+  border: 5px double #8e00e880;
+  border-radius: 0.3rem;
+  transition: background-color ease 0.5s;
+  box-shadow: 0 5px #999;
+
+  &:active {
+    background-color: #d4f285;
+    box-shadow: 0 4px #666;
+    transform: translateY(4px);
+  }
+
+  @media (min-width: 1024px) {
+    &:hover {
+      background-color: #d4f285;
+    }
+  }
+`;
