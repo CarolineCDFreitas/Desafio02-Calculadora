a5d387cbf2335e361355854ce2ec9b11ed343f1c
carolinecdfreitas <117691574+CarolineCDFreitas@users.noreply.github.com>
2024-11-09 21:18:40 +0000

    add: validação final e remoção do manifest .json do html build

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ export default function App() {
   const [segundoValor, setSegundoValor] = useState("");
   const [resultado, setResultado] = useState("");
 
-  //TODO: (outras possibilidades de verificação)
-  //[] Aprender Regex;
-  //[x] Desabilitar os botões com verificação dos valores de entrada!!
+  //TODO:
+  //[]melhorar as verificações
 
-  //função genérica para capturar, filtrar e verificar esses valores, e a seubsequente conversão em número desses valores
+  //função genérica para capturar, converter em número
   const capturandoValores = (e, setValor) => {
     const valor = e.target.value;
 
@@ -19,7 +18,7 @@ export default function App() {
       (caracteres) => !isNaN(caracteres) && caracteres !== ""
     );
 
-    //condicional para verificar se tem número, e, se sim, juntá-los e convertê-los em números para as operações aritméticas.
+    // //condicional para verificar se tem número, e, se sim, juntá-los e convertê-los em números para as operações aritméticas.
     const valorFiltrado = ehNumero.length > 0 ? Number(ehNumero.join("")) : "";
 
     setValor(valorFiltrado);
@@ -33,23 +32,33 @@ export default function App() {
     capturandoValores(e, setSegundoValor);
   };
 
+  //função genérica que valida as entradas para então realizar as operações
+  const validarEntradas = () => {
+    return (
+      primeiroValor !== "" &&
+      segundoValor !== "" &&
+      !isNaN(primeiroValor) &&
+      !isNaN(segundoValor)
+    );
+  };
+
   //funções que validam os valores, realizam as operações aritméticas e depois convertem os resultados para strings
   const soma = () => {
-    if (primeiroValor !== "" && segundoValor !== "") {
+    if (validarEntradas()) {
       const resultadoSoma = Number(primeiroValor) + Number(segundoValor);
       setResultado(String(resultadoSoma));
     }
   };
 
   const subtracao = () => {
-    if (primeiroValor !== "" && segundoValor !== "") {
+    if (validarEntradas()) {
       const resultadoSubtracao = Number(primeiroValor) - Number(segundoValor);
       setResultado(String(resultadoSubtracao));
     }
   };
 
   const multiplicacao = () => {
-    if (primeiroValor !== "" && segundoValor !== "") {
+    if (validarEntradas()) {
       const resultadoMultiplicacao =
         Number(primeiroValor) * Number(segundoValor);
       setResultado(String(resultadoMultiplicacao));
@@ -58,7 +67,7 @@ export default function App() {
 
   //verficação do segundo valor, caso contrário qualquer número dividido por 0 dá como resultado Infinity
   const divisao = () => {
-    if (primeiroValor !== "" && segundoValor !== "") {
+    if (validarEntradas()) {
       const resultadoDivisao = Number(primeiroValor) / Number(segundoValor);
       segundoValor === 0
         ? (alert("Não é possível dividir por 0."), handleClear())
@@ -68,7 +77,7 @@ export default function App() {
 
   //função adicional para realizar a operação de porcentagem
   const porcentagem = () => {
-    if (primeiroValor !== "" && segundoValor !== "") {
+    if (validarEntradas()) {
       const resultadoPorcentagem =
         (Number(primeiroValor) * Number(segundoValor)) / 100;
       setResultado(String(resultadoPorcentagem));
@@ -90,21 +99,19 @@ export default function App() {
         <S.Input
           type="text"
           inputMode="numeric"
-          name="primeiro input"
+          id="primeiroInput"
           value={primeiroValor}
           placeholder="Digite o primeiro número"
           onChange={capturandoPrimeiroValor}
           autoFocus
-          autoComplete="off"
         />
         <S.Input
           type="text"
           inputMode="numeric"
-          name="segundo input"
+          id="segundoInput"
           value={segundoValor}
           placeholder="Digite o segundo número"
           onChange={capturandoSegundoValor}
-          autoComplete="off"
         />
       </S.ConteinerInput>
       <S.ConteinerButton>
@@ -115,7 +122,9 @@ export default function App() {
         <S.Button onClick={divisao}>/</S.Button>
         <S.Button onClick={porcentagem}>%</S.Button>
       </S.ConteinerButton>
-      <S.Resultado>{resultado}</S.Resultado>
+      <S.Resultado htmlFor="primeiroInput segundoInput">
+        {resultado}
+      </S.Resultado>
     </S.Main>
   );
 }
